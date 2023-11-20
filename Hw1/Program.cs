39b2098f7f66e3b72b35d2c4fec90d2014c72ace
Vasul Suberlyak <vasyl.suberlyak@simcorp.com>
2023-11-20 13:50:43 +0100

    introduce an error commit

--- Hw1/Program.cs
+++ Hw1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Vasyl Suberlyak");
 Console.WriteLine("Feature branch 1 - development 1");
+Console.WriteLine("Feature branch 1 - development 2");
