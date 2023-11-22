257328e0f1ce75ea03e308589272d118cef97297
Vasul Suberlyak <vasyl.suberlyak@simcorp.com>
2023-11-22 22:23:34 +0100

    ASSIGNMENT 1

---
+++ Hw2/Calculations.cs
@@ -0,0 +1,40 @@
+﻿
+    public class Calculations
+    {
+        public static int GetNominal()
+        {
+            Console.WriteLine("Input the nominal of the trade:");
+            int nominal = Int32.Parse(Console.ReadLine());
+            return nominal;
+        }
+
+        public static double GetPrice()
+        {
+            Console.WriteLine("Input trade price:");
+            double price = Double.Parse(Console.ReadLine());
+            return price;
+        }
+
+        public static string GetTransactionType()
+        {
+            Console.WriteLine("Input Transaction Type (Buy or Sell) of the trade:");
+            string transType = Console.ReadLine();
+            return transType;
+        }
+
+        public static double CalculateCurrentValue(int a, int b, double c)
+        {
+            double value = a * b * c;
+            return value;
+
+        }
+
+        public static void ShowResult(string transType, int nominal, double price, double currentValue)
+        {
+            Console.Clear();
+            Console.WriteLine($" \n\rTransaction details:\n\rNominal = {nominal}" +
+                              $" \n\rPrice = {price}" +
+                              $" \n\rType  = {transType}" +
+                              $" \n\rCurrent value is: {currentValue}");
+        }
+    }
--- Hw2/Program.cs
+++ Hw2/Program.cs
@@ -1,1 +1,22 @@
-﻿// Homework#2
+﻿using Hw2;
+
+//get nominal of trade
+    
+    int nominal = Calculations.GetNominal();//Int32.Parse(Console.ReadLine());
+
+//get trade price
+    
+    double price = Calculations.GetPrice();
+
+//get Transaction Type for trade
+    
+    string transType = Calculations.GetTransactionType();
+
+    TransactionType trcType;
+    trcType = (TransactionType) Enum.Parse(typeof(TransactionType), transType, true);
+
+// calculate current value     
+    double currentValue = Calculations.CalculateCurrentValue((int)trcType, nominal, price);
+
+// display result for user
+    Calculations.ShowResult(transType, nominal, price, currentValue);
